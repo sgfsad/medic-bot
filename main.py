411b3f441fdf52c31979fd02fce511d748378129
sgfsad <sfgdsfg@gmail.com>
2019-09-09 21:34:00 +0300

    7

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
+import os
 import telebot
-import constants
+from boto.s3.connection import S3Connection
+token = S3Connection(os.environ['token'])
 bot = telebot.TeleBot(token)
 
 bot.send_message(868719453, "test")
